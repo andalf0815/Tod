f2fc9831260a495cac4bec5e2db3dfdd0dd112c4
Andreas Gruber <andreas-gruber@outlook.at>
2023-10-06 13:42:25 +0200

    created id for every todo & filter todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+import { css } from '@emotion/css';
 
 function App() {
-  const [todos, setTodos] = useState([
-    { text: 'Der erste Eintrag', done: true },
-    { text: 'Der zweite Eintrag', done: false },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   return (
     <>
       <div className='app'>
         <TodoInput
           onAddClicked={(todoText) => {
-            setTodos((oldTodos) => [...oldTodos, { text: todoText, done: false }]);
+            setTodos((oldTodos) => [...oldTodos, { id: (Date.now()).toString(), text: todoText, done: false }]);
             console.log(todos);
           }}
         />
         <TodoList
           todos={todos}
-          onDoneChanged={(done, index) => {
+          onDoneChanged={(done, id) => {
             setTodos((oldTodos) =>
-              oldTodos.map((todo, _index) => (index === _index ? Object.assign(todo, { done }) : todo))
+              oldTodos.map((todo) => (id === todo.id ? Object.assign(todo, { done }) : todo))
             );
           }}
         />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,33 @@
 function TodoList(props) {
   return (
     <div>
-      {props.todos.map((todo, index) => (
-        <div key={index}>
-          <input
-            type='checkbox'
-            checked={todo.done}
-            onChange={(event) => {
-              const checked = event.target.checked;
-              props.onDoneChanged && props.onDoneChanged(checked, index);
-            }}
-          ></input>
-          <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>{todo.text}</span>
-        </div>
-      ))}
+      <TodoEntryList todos={props.todos} onDoneChanged={props.onDoneChanged} filterFunction={(todo) => !todo.done}/>
+      <hr />
+      <TodoEntryList todos={props.todos} onDoneChanged={props.onDoneChanged} filterFunction={(todo) => todo.done} />
     </div>
+  );
+}
+
+function TodoEntryList(props) {
+  console.log(props)
+  return (
+    <dir>
+      {props.todos
+        .filter(props.filterFunction)
+        .map((todo) => (
+          <div key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.done}
+              onChange={(event) => {
+                const checked = event.target.checked;
+                props.onDoneChanged && props.onDoneChanged(checked, todo.id);
+              }}
+            ></input>
+            <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>{todo.text}</span>
+          </div>
+        ))}
+    </dir>
   );
 }
 
