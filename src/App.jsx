2f20b2507da8fd11be751d66dc02dc91daf33cae
Andreas Gruber <andreas-gruber@outlook.at>
2023-10-06 11:13:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+function App() {
+
+  return (
+    <>
+      <div className='app'>
+      </div>
+    </>
+  );
+}
+
+export default App;
